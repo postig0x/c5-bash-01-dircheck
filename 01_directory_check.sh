b092e13102e3b33bdb85e509e35dc2baa0d35d51
postig0x <postigo.kura@gmail.com>
2024-08-13 22:58:43 +0000

    added documentation to ex.01

--- 01_directory_check.sh
+++ 01_directory_check.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+# 01 - Directory Check
+# 08.13.24
 # check if a directory named `backup` exists in current directory
 # if it does not exist, create it
 
-# no need to check with -p flag?
-mkdir -p backup
+# SOLUTION 1 - mkdir -p creates dir if it does not exist, no error/no-op if it does
+# mkdir -p backup
 
-# with an if statement
+# SOLUTION 2 - if statement
+# check if backup directory does not exist
 if ! [[ -d "backup" ]]
 then
-  mkdir backup
+  mkdir backup # create directory
 else
-  echo "backup directory exists"
+  echo "backup directory exists" # feedback for existing directory
 fi
