13b3b2df87ff15d738eba93b323b443c7120a019
postig0x <postigo.kura@gmail.com>
2024-08-14 22:15:53 +0000

    02_permission_check added

---
+++ 02_permission_check.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# 02 - Permission Check
+# Check if a file is readable, writable, and executable.
+
+echo "Enter path to file:"
+read filepath
+
+# check if path is a file/exists
+if ! [[ -f $filepath ]]
+then
+  echo "$filepath does not exist"
+  exit 1
+fi
+
+# check if file is readable
+if [[ -r $filepath ]]
+then
+  echo "$filepath is readable"
+else
+  echo "$filepath is not readable"
+fi
+
+# check if file is writable
+if [[ -w $filepath ]]
+then
+  echo "$filepath is writable"
+else
+  echo "$filepath is not writable"
+fi
+
+# check if file is executable
+if [[ -x $filepath ]]
+then
+  echo "$filepath is executable"
+else
+  echo "$filepath is not executable"
+fi
+
